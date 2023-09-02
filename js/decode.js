c5176351dfefa2ff7c4b8292cae31c825f8b2f15
JackCen <ghotm@alioth.cc>
2023-09-02 23:15:42 +0800

    add ScreenShare

--- js/decode.js
+++ js/decode.js
@@ -68,44 +68,68 @@ function parseQRcodeBuffer(qrcodeBuffer) {
     };
 }
 
-const video = document.createElement('video');
-const progressBarCanvas = document.querySelector('#progress-bar');
-const canvas = document.querySelector('#canvas');
-document.body.append(canvas);
-const progressBarCtx = progressBarCanvas.getContext('2d');
-progressBarCtx.fillStyle = '#ccc';
-progressBarCtx.fillRect(0, 0, progressBarCanvas.width, progressBarCanvas.height);
-const ctx = canvas.getContext('2d', {willReadFrequently: true});
-navigator.mediaDevices.getUserMedia({
-    video: {
-        facingMode: 'environment',
-        width: 1920,
-        height: 1080,
-    }
-}).then((stream) => {
-    video.srcObject = stream;
-    video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
-    video.play();
-    let fileName = 'file';
-    let fileData = null;
-    let blockCount = null;
-    let remainingBlockIndexSet = null;
 
-    const tick = () => {
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            const t0 = Date.now();
-            const code = jsQR(imageData.data, imageData.width, imageData.height, {
-                inversionAttempts: 'dontInvert',
-            });
-            const t1 = Date.now();
-            if (code) {
-                drawQrcodeRegion(ctx, code);
-                if (code.binaryData.length > 8) {
-                    const result = parseQRcodeBuffer(new Uint8ClampedArray(code.binaryData).buffer);
+const video = document.createElement('video');
+function start(startType) {
+    // const canvas = document.querySelector('#canvas');
+    // document.body.append(canvas);
+    // const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    let mediaType = {
+        0:{
+            video: {
+                facingMode: 'environment',
+                width: 1920,
+                height: 1080,
+            }
+        },
+        1:{ audio: true }
+    }
+    const handleStream = (stream) => {
+        video.srcObject = stream;
+        video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
+        video.play();
+    }
+    if(startType == 0){
+        navigator.mediaDevices.getUserMedia(mediaType[startType]).then(handleStream);
+    }else{
+        navigator.mediaDevices.getDisplayMedia().then(handleStream);
+    }
+    stopTickVideo = false;
+    tickVideo(true)
+}
+
+let fileName = 'file';
+let fileData = null;
+let blockCount = null;
+let remainingBlockIndexSet = null;
+const canvas = document.querySelector('#canvas');
+const progressBarCanvas = document.querySelector('#progress-bar');
+let stopTickVideo = true;
+const tickVideo = (isRequestAnimationFrame) => {
+    if(stopTickVideo){
+        return;
+    }
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+        const imageData = ctx.getImageData(0, 0, video.videoWidth, video.videoHeight);
+        const t0 = Date.now();
+        const code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: 'dontInvert',
+        });
+        const t1 = Date.now();
+        if (code) {
+            drawQrcodeRegion(ctx, code);
+            if (code.binaryData.length > 8) {
+                new Promise((resolve, reject) => {
+                    let result = parseQRcodeBuffer(new Uint8ClampedArray(code.binaryData).buffer);
+                    if(result){
+                        resolve(result) 
+                    }else{
+                        reject()
+                    }
+                }).then((result) => {
+                    
                     if (result.blockIndex === 0) {
                         if (result.fileName !== fileName || result.fileLength !== fileData.byteLength) {
                             fileName = result.fileName;
@@ -120,30 +144,33 @@ navigator.mediaDevices.getUserMedia({
                             document.querySelector('#speed').textContent = `${t1 - t0}`;
                             document.querySelector('#finished-block-count').textContent = `1`;
                             document.querySelector('#all-block-count').textContent = `${blockCount}`;
-                            progressBarCanvas.width = result.lastBlockIndex + 1;
-                            progressBarCtx.fillStyle = '#ccc';
-                            progressBarCtx.fillRect(0, 0, progressBarCanvas.width, progressBarCanvas.height);
-                            progressBarCtx.fillStyle = '#390';
-                            progressBarCtx.fillRect(result.blockIndex, 0, 1, 1);
+                            progressBarSetProgress(document.querySelector('#progress-bar'), result.blockIndex / blockCount, '#390', '#ccc');
                         }
                     } else {
                         if (fileData) {
                             (new Uint8ClampedArray(fileData.buffer, result.blockOffset, result.blockData.length)).set(result.blockData);
                             remainingBlockIndexSet.delete(result.blockIndex);
                             document.querySelector('#finished-block-count').textContent = `${blockCount - remainingBlockIndexSet.size}`;
-                            progressBarCtx.fillStyle = '#390';
-                            progressBarCtx.fillRect(result.blockIndex, 0, 1, 1);
+                            progressBarSetProgress(document.querySelector('#progress-bar'), result.blockIndex / blockCount, '#390', '#ccc');
                         }
                     }
-                    console.log(fileName, result.blockIndex, remainingBlockIndexSet);
-
-                }
+                    console.log(fileName, result.blockIndex,(remainingBlockIndexSet ? remainingBlockIndexSet.size : 0));
+                })
             }
         }
-        requestAnimationFrame(tick);
     }
-    tick();
+    if(isRequestAnimationFrame){
+        requestAnimationFrame(() => tickVideo(isRequestAnimationFrame));
+    }
+    
+}
 
+function progressBarSetProgress(el, progress, frontColor, backgroundColor) {
+    el.style.background = `linear-gradient(to right, ${frontColor} 0%, ${frontColor} ${progress * 100}%, ${backgroundColor} ${progress * 100}%, ${backgroundColor} 100%)`;
+}
+
+window.onload = function () {
+    document.body.append(canvas);
     document.querySelector('#reset-button').addEventListener('click', () => {
         fileName = 'file';
         fileData = null;
@@ -152,8 +179,38 @@ navigator.mediaDevices.getUserMedia({
     });
     document.querySelector('#download-button').addEventListener('click', () => {
         if (fileData) {
-            const blob = new Blob([fileData], {type: 'application/octet-stream'});
+            const blob = new Blob([fileData], { type: 'application/octet-stream' });
             saveAs(blob, fileName);
         }
     });
-});
+    video.addEventListener("loadedmetadata", () => {
+        document.querySelector('#canvas').width = video.videoWidth;
+        document.querySelector('#canvas').height = video.videoHeight;
+    });
+    video.addEventListener("timeupdate", () => {
+        tickVideo(false);
+    });
+
+    document.querySelector('#start-button').addEventListener('click', () => {
+        const startType = document.querySelector('[name="startType"]:checked').value;
+        if(!startType){
+            alert("请选择启动方式")
+            return;
+        }
+        let btnText = document.querySelector('#start-button').textContent;
+        /* reset btn text */
+        document.querySelector('#start-button').textContent = "Start";
+        if(btnText == "Start"){
+            start(startType);
+            document.querySelector('#start-button').textContent = "Stop";
+            return;
+        }
+        progressBarSetProgress(document.querySelector('#progress-bar'), 0, '#390', '#ccc');
+        video.srcObject = null;
+        fileName = 'file';
+        fileData = null;
+        blockCount = null;
+        remainingBlockIndexSet = null;
+        stopTickVideo = true;
+    });
+}
